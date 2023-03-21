7fd10c931e029858c4992d290c8b28b2b276652c
Yab8702 <yabelectronics@gmail.com>
2023-03-21 12:12:39 -0700

    print some text

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
- * print some text
+ * print some text using non standared library
  * Return: Always 0(sucess)
  */
 int main(void)
 {
-	printf("__putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
