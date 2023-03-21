168d47128087298e7872743dee0746c0727ff9d7
Yab8702 <yabelectronics@gmail.com>
2023-03-21 13:25:35 -0700

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - the absolute value of an integer
+ * @n: the number to check
+ * Return: Always 0(success)
+ */
+
+
+
+int _abs(int n)
+{
+	if (n < 0)
+		n * = -1;
+	return (0);
+}
