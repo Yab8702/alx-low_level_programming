37cc2446b3b9a81aa81f41367a6937248e65e64e
Yab8702 <yabelectronics@gmail.com>
2023-03-21 17:32:53 -0700

    the first 50 Fibonacci numbers start from 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the first 50 fibonacci number
+ * Return:Always 0(sucess)
+ */
+
+int main(void)
+{
+	int x;
+	long int fib1 = 1, fib2 = 1, fib3;
+
+	printf("%ld, ", fib2);
+	for (x = 0; x < 50; x++)
+	{
+		fib3 = fib1 + fib2;
+		printf("%ld", fib3);
+		fib1 = fib2;
+		fib2 = fib3;
+		if (x < 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
