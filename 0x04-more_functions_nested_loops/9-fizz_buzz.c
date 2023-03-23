5214d0449c4127be52126617ef59863994fbb82a
Yab8702 <yabelectronics@gmail.com>
2023-03-23 07:47:04 -0700

    The Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the "Fizz-Buzz test"
+ * Return: Always 0(sucess)
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+			printf("FizzBuzz ");
+		else
+		{
+			if (count % 3 == 0)
+				printf("Fizz ");
+			else if (count % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", count);
+		}
+	}
+	return (0);
+}
