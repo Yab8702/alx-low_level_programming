ccbe28b0d16f31c77d0ddcc7bd040926a5af0fa1
Yab8702 <yabelectronics@gmail.com>
2023-03-27 14:36:55 -0700

    converting string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to integer
+ * @str: string
+ * Return: integer
+ */
+
+int _atoi(char *str)
+{
+	int result = 0, len = 0, sign = 1, i, j;
+
+	while (str[len] != '\0') /* get the length of string*/
+		len++;
+	/* ignoring whitspace and check for sign*/
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '+')
+			continue;
+		else if (str[i] == '-')
+			sign *= -1;
+		else if (str[i] >= '0' && str[i] <= '9')
+			break;
+	}
+	/* converting string to integer */
+
+	for (j = 0; j < len; j++)
+	{
+		if (str[j] >= '0' && str[j] <= '9')
+			result = result * 10 + (str[j] - '0');
+		else if ((j > 0) && (str[j - 1] >= '0' && str[j - 1] <= '9') &&
+			 !(str[j] >= '0' && str[j] <= '9'))
+		{
+			break;
+		}
+		else
+			continue;
+	}
+	return (sign * result);
+}
