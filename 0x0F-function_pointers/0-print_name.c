9a44e41933147194f376742341a7b34b04d6a8cd
Yab8702 <yabelectronics@gmail.com>
2023-04-19 11:12:23 +0000

    function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include<stdio.h>
 
 
 
@@ -11,5 +12,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
