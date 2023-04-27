e2079529dbb54780a8ab73921b111d4d67078408
Yab8702 <yabelectronics@gmail.com>
2023-04-27 20:53:46 +0000

    Singly linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,6 +19,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 	ptr = *head;
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = len;
