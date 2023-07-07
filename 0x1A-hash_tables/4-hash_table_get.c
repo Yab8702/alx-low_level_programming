e0b0a643914404a31043fa24679745e62e7d1fd2
Yab8702 <yaleletdessalegn11@gmail.com>
2023-07-07 09:05:11 +0000

    update retrive value using key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,12 +12,16 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *current = ht->array[index];
+	unsigned long int index;
+	hash_node_t *current;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 	while (current != NULL)
 	{
-		if (current->key == key)
+		if (strcmp(current->key, key) == 0)
 			return (current->value);
 		current = current->next;
 	}
