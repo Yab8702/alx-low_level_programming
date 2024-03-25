d27af41f05943c640515f3a13beb0f336520c19e
yab8702 <yaleletdessalegn11@gmail.com>
2024-03-25 21:00:45 +0300

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/test2/opp.c
@@ -0,0 +1,52 @@
+/**
+ * add - add two numbers
+ * @a: the first number
+ * @b:the second Number
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sum - subtract two number
+ * @a: the first number
+ * @b: the second number
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication of two numbers
+ * @a: the first number
+ * @b: the second Number
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mul - division of two numbers
+ * @a: the first number
+ * @b: the second Number
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modules of two numbers
+ * @a: the first number
+ * @b: the second Number
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
